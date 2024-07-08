1e0e8b37512e17944ab66654b17e695f29315e6a
hhhello <hhhello0507@dgsw.hs.kr>
2024-07-08 13:01:49 +0900

    Remove Socket Rocket

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,43 +9,25 @@ let package = Package(
     products: [
         .library(
             name: "ApeunStompKit",
-            targets: ["ApeunStompKit", "SocketRocket"]
+            targets: ["ApeunStompKit"]
         ),
         .executable(name: "Test", targets: ["Test"])
     ],
+    dependencies: [
+        .package(url: "https://github.com/apeun-gidaechi/SocketRocket.git", exact: "1.0.0")
+    ],
     targets: [
         .target(
-            name: "SocketRocket",
-            path: "SocketRocket",
-            exclude: ["Resources"],
-            sources: [".", "Internal"],
-            publicHeadersPath: "include",
-            cSettings: [
-                .headerSearchPath("."),
-                .headerSearchPath("Internal"),
-                .headerSearchPath("Internal/Delegate"),
-                .headerSearchPath("Internal/IOConsumer"),
-                .headerSearchPath("Internal/Proxy"),
-                .headerSearchPath("Internal/RunLoop"),
-                .headerSearchPath("Internal/Security"),
-                .headerSearchPath("Internal/Utilities")
-            ],
-            linkerSettings: [
-                .linkedFramework("CFNetwork", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("Security"),
-                .linkedFramework("CoreServices", .when(platforms: [.macOS])),
-                .linkedLibrary("icucore")
-            ]
-        ),
-        .target(
             name: "ApeunStompKit",
-            dependencies: ["SocketRocket"],
-            path: "ApeunStompKit"
+            dependencies: [
+                "SocketRocket"
+            ],
+            path: "./ApeunStompKit"
         ),
         .executableTarget(
             name: "Test",
             dependencies: ["ApeunStompKit"],
-            path: "Test"
+            path: "./Test"
         )
     ]
 )
